101776edbe08b464761cd978fcd1594eaeba1a39
Kaykobad <kaykobadreza@gmail.com>
2021-04-22 09:34:02 +0600

    Add all the required urls

--- DjangoSocialMediaBackend/urls.py
+++ DjangoSocialMediaBackend/urls.py
@@ -13,9 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+from configuration.views import ConfigurationViewSet
+from feedback.views import FeedBackViewSet
+from account.views import AuthViewSet, ProfileViewSet
+from friend.views import FriendViewSet
+from feed.views import FeedViewSet
+
+# Configure Site Titles
+admin.site.site_header = "Django Social Media"
+admin.site.site_title = "Django Social Media"
+admin.site.index_title = "Welcome to Admin Portal"
+
+# API Routes
+router = routers.DefaultRouter()
+router.register('api/account/auth', AuthViewSet, basename='authentication')
+router.register('api/account/profile', ProfileViewSet, basename='user_profile')
+router.register('api/configuration', ConfigurationViewSet, basename='configurations')
+router.register('api/feedback', FeedBackViewSet, basename='all_feedback')
+router.register('api/friends', FriendViewSet, basename='friend')
+router.register('api/feed', FeedViewSet, basename='feeds')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', admin.site.urls),
+] + router.urls
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
